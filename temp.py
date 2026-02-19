c2cba0c1eb6152a3faa79b8e38f9130b78d6c4d9
gumba <gumba6740@gmail.com>
2026-02-19 20:53:30 +0900

    Initial Commit

--- temp.py
+++ temp.py
@@ -1,6 +1,2 @@
-my_tuple: tuple[str, ...] = ("a", "b")
-print(my_tuple)
-
-my_dict: dict[str, int] = {"a": 1, "b": 2, "c": 3}
-
-or_type_list: list[str | bool] = ["a", "b", "c", True]
+def add(a: int, b: int) -> int:
+    return a + b
---
+++ test_temp.py
@@ -0,0 +1,8 @@
+from temp import add
+
+
+def test_add() -> None:
+    a, b = 1, 1
+    result = add(a, b)
+
+    assert result == a + b
