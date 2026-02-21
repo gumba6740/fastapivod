57b78a65780ba9538259565cfe11c012708e8881
gumba <gumba6740@gmail.com>
2026-02-21 11:05:28 +0900

    add base62

---
+++ app/utils/base62.py
@@ -0,0 +1,23 @@
+import string
+
+# import uuid
+from typing import Final
+
+
+class Base62:
+    BASE: Final[str] = string.ascii_letters + string.digits
+    BASE_LEN: Final[int] = len(BASE)
+
+    @classmethod
+    def encode(cls, num: int) -> str:
+        if num < 0:
+            raise ValueError(f"{cls}.encode() needs positive integer but you passed: {num}")
+
+        result = []
+        while num:
+            num, remainder = divmod(num, cls.BASE_LEN)
+            result.append(cls.BASE[remainder])
+        return "".join(result)
+
+
+# print(Base62.encode(uuid.uuid4().int))
